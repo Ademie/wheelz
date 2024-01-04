09e7c0bbb151ad0822b4df85c7044a9441c9e1ad
ademie <isijolaademie@gmail.com>
2024-01-04 10:40:46 -0600

    added drawer for user

--- lib/user/pages/user_home.dart
+++ lib/user/pages/user_home.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:wheelz/global/global_var.dart';
+import 'package:wheelz/methods/common_methods.dart';
+import 'package:wheelz/user/authentication/user_login.dart';
 
 class UserHome extends StatefulWidget {
   const UserHome({super.key});
@@ -19,6 +23,8 @@ class _UserHomeState extends State<UserHome> {
       Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
   Position? currentPositionOfUser;
+  GlobalKey<ScaffoldState> sKey = GlobalKey<ScaffoldState>();
+  CommonMethods cMethods = CommonMethods();
 
   void updateMapTheme(GoogleMapController controller) {
     getJsonFileFromThemes("themes/retro_style.json")
@@ -56,23 +62,198 @@ class _UserHomeState extends State<UserHome> {
         CameraPosition(target: positionOfUserInLatLng, zoom: 15);
     controllerGoogleMap!
         .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    await getUserInfoAndCheckBlockStatus();
+  }
+
+  getUserInfoAndCheckBlockStatus() async {
+    DatabaseReference usersRef = FirebaseDatabase.instance
+        .ref()
+        .child("users")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+    await usersRef.once().then((snap) {
+      if (snap.snapshot.value != null) {
+        if ((snap.snapshot.value as Map)["blockStatus"] == "no") {
+          setState(() {
+            userName = (snap.snapshot.value as Map)["name"];
+          });
+        } else {
+          FirebaseAuth.instance.signOut();
+
+          Navigator.push(
+              context, MaterialPageRoute(builder: (c) => const UserLogin()));
+
+          cMethods.displaySnackBar(
+              "you are blocked. Contact admin: isijolaademie@gmail.com",
+              context);
+        }
+      } else {
+        FirebaseAuth.instance.signOut();
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => const UserLogin()));
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: sKey,
+      drawer: Container(
+        width: 255,
+        color: Colors.black87,
+        child: Drawer(
+          backgroundColor: Colors.white10,
+          child: ListView(
+            children: [
+              const Divider(
+                height: 1,
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              //header
+              Container(
+                color: Colors.black54,
+                height: 160,
+                child: DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.white10,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "assets/images/avatarman.png",
+                        width: 60,
+                        height: 60,
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            userName,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          const Text(
+                            "Profile",
+                            style: TextStyle(
+                              color: Colors.white38,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const Divider(
+                height: 1,
+                color: Colors.grey,
+                thickness: 1,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              //body
+              ListTile(
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.info,
+                    color: Colors.grey,
+                  ),
+                ),
+                title: const Text(
+                  "About",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+
+              GestureDetector(
+                onTap: () {
+                  FirebaseAuth.instance.signOut();
+
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (c) => const UserLogin()));
+                },
+                child: ListTile(
+                  leading: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  title: const Text(
+                    "Logout",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Stack(
         children: [
+          //google map
           GoogleMap(
+            padding: const EdgeInsets.only(top: 26),
             mapType: MapType.normal,
             myLocationEnabled: true,
             initialCameraPosition: googlePlexInitialPosition,
             onMapCreated: (GoogleMapController mapController) {
               controllerGoogleMap = mapController;
               updateMapTheme(controllerGoogleMap!);
+
               googleMapCompleterController.complete(controllerGoogleMap);
+
               getCurrentLiveLocationOfUser();
             },
+          ),
+
+          //drawer button
+          Positioned(
+            top: 36,
+            left: 19,
+            child: GestureDetector(
+              onTap: () {
+                sKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    ),
+                  ],
+                ),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
